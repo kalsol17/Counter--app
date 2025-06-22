0e7802dba75e04cb3abc51ca570ae64f44e30f04
kaleab solomon <kaleab24gebreselassie@gmail.com>
2025-06-22 13:24:01 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
-
-  @override
-  State<MyWidget> createState() => _MyWidgetState();
+void main(){
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+    home:Homepage()
+    ));
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => HomepageState();
+}
+
+class HomepageState extends State<Homepage> {
+  int counter=0;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('A counter App that increases when the button is Clicked.',
+            style: TextStyle(fontWeight: FontWeight.bold),),
+            Text('You pressed $counter times'),
+            SizedBox(height: 10,),
+            ElevatedButton(onPressed: (){
+              setState(() {
+                counter ++;
+              },);
+            }, 
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+            child: Text('Press me',style: TextStyle(color:Colors.white,fontSize: 20))
+            ),
+          ],),),
+    );
   }
 }
